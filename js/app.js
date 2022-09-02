066f2ccf8f1d7752a5a63ff6e813e2f5cd5ee905
Dante Roldan <danteroldan72@gmail.com>
2022-09-02 14:28:08 -0300

    cambio

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+
+
+// const api = "http://localhost:3000/shop";
+
+// fetch(api)
+// .then(response => response.json())
+// .then(data => {
+   
+//     let nombres = "";
+//     data.forEach(function (data) {
+
+//         nombres +=  ` <h1 class="px-2"> ${data.nombre} </h1> `;
+//         // nombres +=  ` <h1> ${data.address} </h1>`;
+
+        
+//         document.querySelector('#name').innerHTML = nombres;
+
+//     });
+
+
+// })
+// .catch(err => console.log(err) )
